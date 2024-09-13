f5e1639ca7557eaad1602eb1b250e6222a2125fe
QiaoEnLiu <137969620+QiaoEnLiu@users.noreply.github.com>
2024-09-13 15:48:31 +0800

    2024 0913 1550

--- readJson.py
+++ readJson.py
@@ -6,6 +6,66 @@ theStop = Stop()
 pathDir = FilePath("臺中市市區公車站細部資訊", "JSON").path()    
 busList = theStop.readJson_File(pathDir)
 
+#region Find Stop
+'''
 for stop in busList:
-    if stop[theStop.stopName_CN] == "三民林森路口":
+    if stop[theStop.stopName_CN] == "吉峰東自強路口":
         print(stop)
+'''
+#endregion
+
+#region Find BusInfo
+
+'''
+for stop in busList:
+    if "132" in stop[theStop.roundTrip_ob] or "132" in stop[theStop.roundTrip_ib]:
+        print(stop)
+'''
+
+#endregion
+
+#region Take Bus(Direct)
+
+
+takeStop = "國立臺中科技大學"
+desStop = "朝陽科技大學" #國立臺中科技大學 吉峰東自強路口 朝陽科技大學
+
+takeInfo = []
+for stop in busList:
+    if stop[theStop.stopName_CN] == takeStop:
+        takeInfo.append(stop)
+
+desInfo = []
+for stop in busList:
+    if stop[theStop.stopName_CN] == desStop:
+        desInfo.append(stop)
+
+
+
+for takeBus in takeInfo:
+    for desStop in desInfo:
+        
+        sameBusesOB = set(takeBus[theStop.roundTrip_ob].keys()) & set(desStop[theStop.roundTrip_ob].keys())
+        if sameBusesOB:
+            for bus in sameBusesOB:
+                if int(takeBus[theStop.roundTrip_ob][bus][1]) < int(desStop[theStop.roundTrip_ob][bus][1]):
+                    print(takeBus[theStop.roundTrip_ob][bus], desStop[theStop.roundTrip_ob][bus])
+
+        sameBusesIB = set(takeBus[theStop.roundTrip_ib].keys()) & set(desStop[theStop.roundTrip_ib].keys())
+        if sameBusesIB:
+            for bus in sameBusesIB:
+                if int(takeBus[theStop.roundTrip_ib][bus][1]) < int(desStop[theStop.roundTrip_ib][bus][1]):
+                    print(takeBus[theStop.roundTrip_ib][bus], desStop[theStop.roundTrip_ib][bus])
+    
+        '''
+        print(takeBus, desStop, sep = '\n')
+        print("-------")
+        print("RT_OB: ", set(takeBus[theStop.roundTrip_ob].keys()) & set(desStop[theStop.roundTrip_ob].keys()))
+        print("RT_IB: ", set(takeBus[theStop.roundTrip_ib].keys()) & set(desStop[theStop.roundTrip_ib].keys()))
+        print("--------------")
+        input("Press Enter")
+        '''
+        
+
+#endregion
+
--- stopInfoSimple.py
+++ stopInfoSimple.py
@@ -12,33 +12,32 @@ import json
 theStop = Stop()
 
 
-if __name__ =='__main__':
-    pathDir = FilePath("臺中市市區公車站牌資料", "CSV").path()    
-    busList = theStop.readFile(pathDir)
+pathDir = FilePath("臺中市市區公車站牌資料", "CSV").path()    
+busList = theStop.readFile(pathDir)
     
     
-    stopInfo = []    
-    for bus in busList:
+stopInfo = []    
+for bus in busList:
         
         
-        if not any(theStop.stopExist(bus, stop) for stop in stopInfo):
+    if not any(theStop.stopExist(bus, stop) for stop in stopInfo):
             
-            tempDict = theStop.stopInfoDict(bus)                
-            stopInfo.append(tempDict)
+        tempDict = theStop.stopInfoDict(bus)                
+        stopInfo.append(tempDict)
                         
             
-        for stop in stopInfo:
+    for stop in stopInfo:
             
-            if theStop.stopExist(bus, stop):
+        if theStop.stopExist(bus, stop):
                 
-                if bus[theStop.busID] not in stop[bus[theStop.roundTrip]]:
+            if bus[theStop.busID] not in stop[bus[theStop.roundTrip]]:
                     
-                    # stop[bus[theStop.roundTrip]].append(bus[theStop.busID] + "_" + bus[theStop.stopID])
-                    stop[bus[theStop.roundTrip]].update({bus[theStop.busID] : [bus[theStop.busID], bus[theStop.stopID]]})
+                # stop[bus[theStop.roundTrip]].append(bus[theStop.busID] + "_" + bus[theStop.stopID])
+                stop[bus[theStop.roundTrip]].update({bus[theStop.busID] : [bus[theStop.busID], bus[theStop.stopID]]})
                        
 
-    with open(FilePath("臺中市市區公車站細部資訊", "JSON").path(), "w", encoding="utf-8") as jsonfile:
-        json.dump(stopInfo, jsonfile, ensure_ascii=False, indent=4)
+with open(FilePath("臺中市市區公車站細部資訊", "JSON").path(), "w", encoding="utf-8") as jsonfile:
+    json.dump(stopInfo, jsonfile, ensure_ascii=False, indent=4)
                 
         
             
